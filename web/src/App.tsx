fe84e32aa2524834383efb5c4060ef4eb0a8db1c
Victor Pereira <victor.cordeiro2@gmail.com>
2021-01-09 12:32:03 -0300

    Usando rotas e StepsHeader

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
 
-import Navbar from './Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
-      <>
-        <Navbar/>
-        <Home/>
-      </>
-    )
+    <>
+      <Routes />
+    </>
+  )
 }
 
 export default App;
