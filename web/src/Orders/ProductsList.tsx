f914a8875099017740666048773915d4b70f7b69
Victor Pereira <victor.cordeiro2@gmail.com>
2021-01-09 16:19:56 -0300

    Selecionando pedido e gerando resumo

--- web/src/Orders/ProductsList.tsx
+++ web/src/Orders/ProductsList.tsx
@@ -1,16 +1,24 @@
+import { checkIsSelected } from "./helpers";
 import ProductCard from "./ProductCard";
 import { Product } from "./types";
 
 type Props = {
     products: Product[];
+    selectedProducts: Product[];
+    onSelectProduct: (product: Product) => void;
 }
 
-function ProductsList({ products }: Props) {
+function ProductsList({ products, selectedProducts, onSelectProduct }: Props) {
     return (
         <div className="orders-list-container">
             <div className="orders-list-items">
                 {products.map(p => (
-                    <ProductCard key={p.id} product={p}/>
+                    <ProductCard
+                        key={p.id}
+                        product={p}
+                        onSelectProduct={onSelectProduct}
+                        isSelected={checkIsSelected(selectedProducts, p)}
+                    />
                 ))}
             </div>
         </div>
