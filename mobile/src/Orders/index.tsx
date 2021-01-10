bbf61b7af5e94454d5c6d9fe95b35c5d8f4cd3ea
Victor Pereira <victor.cordeiro2@gmail.com>
2021-01-10 16:07:43 -0300

    Confirmando pedido como entregue

--- mobile/src/Orders/index.tsx
+++ mobile/src/Orders/index.tsx
@@ -1,20 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text } from 'react-native';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { Order } from '../types';
 
 export default function Orders() {
+    const navigation = useNavigation();
+    const [orders, setOrders] = useState<Order[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const isFocused = useIsFocused();
+
+
+    const fetchData = () => {
+        setIsLoading(true);
+        fetchOrders()
+            .then(response => setOrders(response.data))
+            .catch(() => Alert.alert('Hove um erro ao buscar os pedidos.'))
+            .finally(() => setIsLoading(false));
+    }
+
+    useEffect(() => {
+        if (isFocused) {
+            fetchData();
+        }
+    }, [isFocused]);
+
+    const handleOnPress = (order: Order) => {
+        navigation.navigate('OrderDetails', { order });
+    }
+
     return (
         <>
             <Header />
             <ScrollView style={styles.container}>
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
+                {isLoading ? (
+                    <Text>Buscando pedidos...</Text>
+                ) : (
+                        orders.map(o => (
+                            <TouchableWithoutFeedback
+                                key={o.id}
+                                onPress={() => { handleOnPress(o) }}
+                            >
+                                <OrderCard order={o} />
+                            </TouchableWithoutFeedback>
+                        ))
+                    )}
             </ScrollView>
         </>
     );
